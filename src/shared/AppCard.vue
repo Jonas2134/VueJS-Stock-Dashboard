75907549d02dc169781a31d8203d3a21a36bb088
Jonas2134 <neo.stief.jon@gmail.com>
2025-03-21 21:49:52 +0100

    add topoverview, appcard and viewcard

---
+++ src/shared/AppCard.vue
@@ -0,0 +1,40 @@
+<template lang="html">
+
+  <section class="app-card">
+    <h1>app-card Component</h1>
+  </section>
+
+</template>
+
+<script lang="js">
+
+export default {
+  name: 'AppCard',
+  props: [],
+  mounted() {
+
+  },
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+
+  },
+  computed: {
+
+  }
+}
+
+
+</script>
+
+<style scoped lang="scss">
+.app-card {
+  display: flex;
+  border-radius: 16px;
+  background-color: #011F35;
+  padding: 20px 32px;
+}
+</style>
---
+++ src/shared/AppViewCard.vue
@@ -0,0 +1,40 @@
+<template lang="html">
+
+  <section class="app-view-card">
+    <h1>app-view-card Component</h1>
+  </section>
+
+</template>
+
+<script lang="js">
+
+export default {
+  name: 'AppViewCard',
+  props: [],
+  mounted() {
+
+  },
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+
+  },
+  computed: {
+
+  }
+}
+
+
+</script>
+
+<style scoped lang="scss">
+.app-view-card {
+  display: flex;
+  border-radius: 16px;
+  padding: 20px 24px;
+  background-color: #011F35;
+}
+</style>
